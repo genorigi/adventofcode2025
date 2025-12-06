b065a313eba2af8e4790a03aa7c6e6cc4c4ff052
Jean Poizat <jean.poizat@se.com>
2025-12-06 15:30:08 +0100

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,3 @@
-use regex::Regex;
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
@@ -9,14 +8,32 @@ use std::str;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut power: i32 = 0;
+    const RADIX: u32 = 10;
+    let mut power: u64 = 0;
     let filename = &args[1];
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines.flatten() {
-            let mut line_power: i32 = 0;
+            let mut line_power: u64 = 0;
+            let line_of_battery: Vec<u32> = line.chars().map(|c| c.to_digit(RADIX).unwrap()).collect();
+            let mut max_index: usize = 0;
+            for i in (0..12).rev() {
+                let mut max_value: u32 = 0;
+                let mut new_index: usize = max_index;
+                for (index, value)  in line_of_battery[max_index..line_of_battery.len()-i].iter().enumerate() {
+                    if *value > max_value {
+                        max_value = *value;
+                        new_index = index + max_index + 1;
+                        //println!("found {} at {}", value, new_index)
+                    }
+                    //println!("line of battery {}", value);
+                }
+                line_power +=  max_value as u64 * 10_u64.pow(i.try_into().unwrap());
+                max_index = new_index;
+            }
+            
             power += line_power;
-            print!("max power is {}", line_power);
+            println!("max power is {}", line_power);
         }
 
     }
